2aacf93573fbdff7eb160c6d422017f9b871bf96
NtuthukoLu <ntuthukolu@gmail.com>
2023-08-27 02:46:34 +0200

    sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,15 @@
 --test MYSQL server--
+-- project by adding a database and new user for
+-- use with testing.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+            IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.*
+          TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON `performance_schema`.*
+          TO 'hbnb_test'@'localhost';
+
 FLUSH PRIVILEGES;
